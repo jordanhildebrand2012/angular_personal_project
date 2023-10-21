4ace7cdb3ed0eaf5d6b9c5d7f2ea8a23d9cfe752
Jordan Shayo <jordanhildebrand2012@gmail.com>
2023-10-21 11:50:39 +0100

    playing around with data binding to the button

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
+  allowServer: boolean = false;
 
+  constructor() {
+    setTimeout(() => {
+      this.allowServer = true;
+    }, 3000);
+  }
 }
