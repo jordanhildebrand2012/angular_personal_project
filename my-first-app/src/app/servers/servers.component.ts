758714fc392cc9e2d8f6040d22f1110cd4e710fe
Jordan Shayo <jordanhildebrand2012@gmail.com>
2023-10-25 13:28:36 +0100

    checking how event binding work

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowServer: boolean = false;
+  serverCreationStatus: string = 'At the moment, server is not created!';
 
   constructor() {
     setTimeout(() => {
       this.allowServer = true;
     }, 3000);
+  }
+
+  onServerCreation() {
+    this.serverCreationStatus = 'Now server has been created, after the click!';
   }
 }
