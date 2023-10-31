8278f6eb51388813c759f5f4c76368a84364d398
Jordan Shayo <jordanhildebrand2012@gmail.com>
2023-10-31 16:49:59 +0000

    create a quick bootrap header

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('tomatoes', 'you will need this for pasta', ''),
+  ];
 }
