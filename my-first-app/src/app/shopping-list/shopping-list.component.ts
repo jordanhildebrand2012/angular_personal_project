a39ccec2398a18e071964c148e290492a90d49dc
Jordan Shayo <jordanhildebrand2012@gmail.com>
2023-11-02 10:22:05 +0000

    edit shopping list component and populate ingredients

--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('apple', 5),
+    new Ingredient('tomatoes', 10),
+    new Ingredient('pasta', 2),
+  ];
 }
